c4bcb2cedc32afa1559df3d5ec2c004cd3f2e0a6
Mihai Manole <mesterum@users.noreply.github.com>
2024-05-08 17:26:58 +0000

    styled NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,34 @@
-// import PropTypes from "prop-types";
-
 import { NavLink, Outlet } from "react-router-dom";
+import styled from 'styled-components';
 
-// import { Blocks } from 'react-loader-spinner'
-//   ;
-// type Props = { visible: boolean }
-
+const NavBarStyle = styled.nav`
+  display: flex;
+  padding: 1rem;
+  column-gap: 1rem;
+  // background-color: #eee;
+  border: 1px solid #e0e0e0;
+  box-shadow: 0 2px 4px rgba(0,0,0,.1);
+  margin-bottom: .5rem;
+  list-style: none;
+  >li>a {
+    text-decoration: none;
+    font-weight: bold;
+    color: black;
+    &:hover {
+      text-decoration: underline;
+    }
+    &.active {
+      color: #b14;
+    }
+  }
+`;
 
 export default function NavBar() {
   return <>
-    <ul>
+    <NavBarStyle>
       <li><NavLink to="/">Home</NavLink></li>
       <li><NavLink to="/movies">Movies</NavLink></li>
-    </ul>
+    </NavBarStyle >
     <Outlet />
   </>
 }
-
-NavBar.propTypes = {
-  // visible: PropTypes.bool.isRequired,
-};
